3cf3e456dd802908c94dcd301f8adbf0543071e4
Vink77 <vink.av@mail.ru>
2023-08-02 18:03:35 +0700

    скопирован из pgAdmin sql-код для создания таблиц и начат скрипт main

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id serial PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(100) PRIMARY KEY,
+    company_name varchar(100),
+	contact_name varchar(100)
+
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+def main() -> None:
+    conn = psycopg2.connect(
+    host = 'localhost',
+    database = 'north',
+    user = 'postgres',
+    password = '9877'
+    )
+    cur = conn.cursor()
+
+    # Считывание данных из CSV файла
+    with open("north_data/customers_data.csv", encoding='utf-8') as r_file:
+        # Создаем объект reader, указываем символ-разделитель ","
+        file_reader = csv.reader(r_file, delimiter=",")
+        cur.executemany("INSERT INTO customers VALUES (%s, %s, %s)", file_reader)
+        cur.execute("SELECT * FROM customers")
+        conn.commit()
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        print(len((rows)))
+
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
