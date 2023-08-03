4324ba952a62b1a9e1a729fc4e944f18fbd5cde7
Vink77 <vink.av@mail.ru>
2023-08-03 14:03:19 +0700

    simple_queries.sql its ready filter_sorting.sql its not ready

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -1,10 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
-SELECT ...
-
+SELECT *
+FROM orders
+WHERE ship_country IN ('France', 'Germany','Spain');
 -- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
-
-
+SELECT DISTINCT ship_country, ship_city
+FROM orders
+ORDER BY ship_country, ship_city;
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 
 
