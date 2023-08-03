15710d9f197d7061fd2362df8a645d87d61d3875
Vink77 <vink.av@mail.ru>
2023-08-03 17:33:57 +0700

    filter_sorting.sql its ready

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -8,9 +8,14 @@ SELECT DISTINCT ship_country, ship_city
 FROM orders
 ORDER BY ship_country, ship_city;
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
-
-
+SELECT AVG(shipped_date - order_date)
+FROM orders
+WHERE ship_country IN ('Germany');
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
-
-
+SELECT MIN(unit_price), MAX (unit_price)
+FROM products
+WHERE discontinued <>1;
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+SELECT MIN(unit_price), MAX (unit_price)
+FROM products
+WHERE discontinued <>1 AND units_in_stock > 20;
